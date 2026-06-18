93fd582c6ae76ec0c0d55e9761c8d412ed8eaf8b
Hubert Rykala <hubert.rykala@gmail.com>
2026-06-18 08:15:56 +0200

    update

--- src/app/shared/ui/sidebar/sidebar.service.ts
+++ src/app/shared/ui/sidebar/sidebar.service.ts
@@ -1,10 +1,6 @@
-import { effect, Injectable, signal } from '@angular/core';
+import { Injectable, signal } from '@angular/core';
 
 @Injectable({ providedIn: 'root' })
 export class SidebarService {
   public readonly isSidebarOpen = signal(false);
-
-  constructor() {
-    effect(() => console.log('iS Sidebar OPen -> ', this.isSidebarOpen()));
-  }
 }
